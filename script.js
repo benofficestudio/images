0f03111fda06b3cc03fd0ce8a5f9d0c890f43ee2
benofficestudio <103466026+benofficestudio@users.noreply.github.com>
2024-03-09 21:02:39 +0000

    Changed background colour to white and added Herman Miller Chair img

--- script.js
+++ script.js
@@ -290,6 +290,15 @@ img.onclick = function(){
   captionText.innerHTML = this.alt;
 }
 
+var img = document.getElementById("myImg33");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
